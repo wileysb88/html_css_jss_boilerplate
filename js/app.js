2167e29ca1e7a4e621672a5d2ec4b2f9c98e6670
Scott Wiley <wileysb@gmail.com>
2016-02-25 11:07:32 -0600

    almost there

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// we want to wait for all the thing
+// on the page to load before doing anything!
+
+window.onload = function(event){
+
+  //anything called or done inside of here
+  // will only occur after the page has loaded
+  // every image, file, style, etc.
+  console.log('hey friends! we made it :)')
+}; // end window.onload
